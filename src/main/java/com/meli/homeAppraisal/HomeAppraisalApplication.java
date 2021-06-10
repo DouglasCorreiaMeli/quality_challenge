7fb5919f44c6990d211b4c740140f1b1b9822ed9
DouglasCorreiaMeli <douglas.correia@mercadolivre.com>
2021-06-10 16:49:17 -0300

    init challenge

---
+++ src/main/java/com/meli/homeAppraisal/HomeAppraisalApplication.java
@@ -0,0 +1,13 @@
+package com.meli.homeAppraisal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeAppraisalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeAppraisalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meli/homeAppraisal/HomeAppraisalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.homeAppraisal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeAppraisalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
